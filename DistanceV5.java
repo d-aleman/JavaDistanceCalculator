66ba47f686e25fce59005abb7a7023bf798490f6
Dionne Aleman <aleman@mie.utoronto.ca>
2022-07-25 16:23:24 -0400

    Initial commit

---
+++ DistanceV5.java
@@ -0,0 +1,13 @@
+import java.io.*; // Needed for exception handling
+
+public class DistanceV5 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		DistanceCalculator dc = new DistanceCalculator();
+		dc.getUserInput();
+		dc.printDistance();
+		
+	} // end of main function
+		
+} // end of class
