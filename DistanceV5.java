520ac2ca8237e58e0e759e03a9f987c8a59b9a26
Dionne Aleman <aleman@mie.utoronto.ca>
2022-07-26 09:28:39 -0400

    Remove program title

--- DistanceV5.java
+++ DistanceV5.java
@@ -2,8 +2,6 @@ public class DistanceV5 {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("A distance calculator in Java\n");
-		
 		DistanceCalculator dc = new DistanceCalculator();
 		dc.getUserInput();
 		dc.printDistance();
