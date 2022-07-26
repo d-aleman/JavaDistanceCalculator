7822a96f7a8f88745deb9671cfb1c87642a2969f
Dionne Aleman <aleman@mie.utoronto.ca>
2022-07-26 13:26:51 -0400

    Title added, with note of conflict

--- DistanceV5.java
+++ DistanceV5.java
@@ -5,6 +5,8 @@ public class DistanceV5 {
 		DistanceCalculator dc = new DistanceCalculator();
         boolean doContinue = true;
         
+        System.out.println("A distance calculator in Java that creates a conflict\n");
+        
         do {
             System.out.println("\n-----------------------------------------------------");
             System.out.println("Distance calculator (To quit, enter a unit then enter 0 for both values)");
