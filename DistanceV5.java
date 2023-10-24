fd8969a39dacae9d33e25a3a9f69bdb62823f552
Dionne Aleman <aleman@mie.utoronto.ca>
2023-10-24 12:59:35 -0400

    Program title added

--- DistanceV5.java
+++ DistanceV5.java
@@ -5,6 +5,8 @@ public class DistanceV5 {
 		DistanceCalculator dc = new DistanceCalculator();
         boolean doContinue = true;
         
+        System.out.println("A distance calculator in Java");
+        
         do {
             System.out.println("\n-----------------------------------------------------");
             System.out.println("Distance calculator (enter 0 for both values to quit)");
